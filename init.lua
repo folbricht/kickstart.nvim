51ce5de6b2df4a1f7f515d6186a9339aaf22f636
folbrich <frank.olbricht@gmail.com>
2024-09-01 21:01:08 +0200

    treesiter text objects

--- init.lua
+++ init.lua
@@ -893,6 +893,9 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go' },
       -- Autoinstall languages that are not installed
@@ -905,6 +908,15 @@ require('lazy').setup({
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
+          scope_incremental = false,
+          node_decremental = '<BS>',
+        },
+      },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -1000,6 +1012,113 @@ require('lazy').setup({
         }
       end,
     },
+  },
+
+  -- Text objects
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    lazy = true,
+    enabled = true,
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('nvim-treesitter.configs').setup {
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ['a='] = { query = '@assignment.outer', desc = 'Select outer part of an assignment' },
+              ['i='] = { query = '@assignment.inner', desc = 'Select inner part of an assignment' },
+              ['l='] = { query = '@assignment.lhs', desc = 'Select left hand side of an assignment' },
+              ['r='] = { query = '@assignment.rhs', desc = 'Select right hand side of an assignment' },
+
+              ['a:'] = { query = '@property.outer', desc = 'Select outer part of an object property' },
+              ['i:'] = { query = '@property.inner', desc = 'Select inner part of an object property' },
+              ['l:'] = { query = '@property.lhs', desc = 'Select left part of an object property' },
+              ['r:'] = { query = '@property.rhs', desc = 'Select right part of an object property' },
+
+              ['aa'] = { query = '@parameter.outer', desc = 'Select outer part of a parameter/argument' },
+              ['ia'] = { query = '@parameter.inner', desc = 'Select inner part of a parameter/argument' },
+
+              ['ai'] = { query = '@conditional.outer', desc = 'Select outer part of a conditional' },
+              ['ii'] = { query = '@conditional.inner', desc = 'Select inner part of a conditional' },
+
+              ['al'] = { query = '@loop.outer', desc = 'Select outer part of a loop' },
+              ['il'] = { query = '@loop.inner', desc = 'Select inner part of a loop' },
+
+              ['af'] = { query = '@call.outer', desc = 'Select outer part of a function call' },
+              ['if'] = { query = '@call.inner', desc = 'Select inner part of a function call' },
+
+              ['am'] = { query = '@function.outer', desc = 'Select outer part of a method/function definition' },
+              ['im'] = { query = '@function.inner', desc = 'Select inner part of a method/function definition' },
+
+              ['ac'] = { query = '@class.outer', desc = 'Select outer part of a class' },
+              ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class' },
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']f'] = { query = '@call.outer', desc = 'Next function call start' },
+              [']m'] = { query = '@function.outer', desc = 'Next method/function def start' },
+              [']c'] = { query = '@class.outer', desc = 'Next class start' },
+              [']i'] = { query = '@conditional.outer', desc = 'Next conditional start' },
+              [']l'] = { query = '@loop.outer', desc = 'Next loop start' },
+              [']s'] = { query = '@scope', query_group = 'locals', desc = 'Next scope' },
+              [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
+            },
+            goto_next_end = {
+              [']F'] = { query = '@call.outer', desc = 'Next function call end' },
+              [']M'] = { query = '@function.outer', desc = 'Next method/function def end' },
+              [']C'] = { query = '@class.outer', desc = 'Next class end' },
+              [']I'] = { query = '@conditional.outer', desc = 'Next conditional end' },
+              [']L'] = { query = '@loop.outer', desc = 'Next loop end' },
+            },
+            goto_previous_start = {
+              ['[f'] = { query = '@call.outer', desc = 'Prev function call start' },
+              ['[m'] = { query = '@function.outer', desc = 'Prev method/function def start' },
+              ['[c'] = { query = '@class.outer', desc = 'Prev class start' },
+              ['[i'] = { query = '@conditional.outer', desc = 'Prev conditional start' },
+              ['[l'] = { query = '@loop.outer', desc = 'Prev loop start' },
+            },
+            goto_previous_end = {
+              ['[F'] = { query = '@call.outer', desc = 'Prev function call end' },
+              ['[M'] = { query = '@function.outer', desc = 'Prev method/function def end' },
+              ['[C'] = { query = '@class.outer', desc = 'Prev class end' },
+              ['[I'] = { query = '@conditional.outer', desc = 'Prev conditional end' },
+              ['[L'] = { query = '@loop.outer', desc = 'Prev loop end' },
+            },
+          },
+        },
+      }
+
+      local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+      -- vim way: ; goes to the direction you were moving.
+      vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move)
+      vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_opposite)
+
+      -- When in diff mode, we want to use the default
+      -- vim text objects c & C instead of the treesitter ones.
+      local move = require 'nvim-treesitter.textobjects.move' ---@type table<string,fun(...)>
+      local configs = require 'nvim-treesitter.configs'
+      for name, fn in pairs(move) do
+        if name:find 'goto' == 1 then
+          move[name] = function(q, ...)
+            if vim.wo.diff then
+              local config = configs.get_module('textobjects.move')[name] ---@type table<string,string>
+              for key, query in pairs(config or {}) do
+                if q == query and key:find '[%]%[][cC]' then
+                  vim.cmd('normal! ' .. key)
+                  return
+                end
+              end
+            end
+            return fn(q, ...)
+          end
+        end
+      end
+    end,
   },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
